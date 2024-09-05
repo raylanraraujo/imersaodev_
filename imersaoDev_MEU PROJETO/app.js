0d76493e3704fb7e3ab78829e7b710a0b9cf2112
Raylan Araujo <raylanra@gmail.com>
2024-09-05 17:04:01 -0300

    incluido a pasta MEU PROJETO a mesma pasta

---
+++ imersaoDev_MEU PROJETO/app.js
@@ -0,0 +1,44 @@
+function pesquisar() {
+    // Obtém a seção HTML onde os resultados da pesquisa serão renderizados
+    let section = document.getElementById("resultados-pesquisa")
+
+    // String que armazenará o HTML gerado para todos os resultados
+    let resultado = ""
+
+    // Itera sobre cada objeto de música no array de dados
+    for (let dado of dados) {
+        // Cria um novo elemento div para cada resultado
+        resultado += `
+            <div class="resultado">
+                <div id="infmusic" class="flex-container">
+                    <div class="imgmusic">
+                        <img class="music" src="${dado.imagem}" alt=""> 
+                    </div>
+                    
+                    <ul class="detmusic">
+                        <li><strong>Título da música:</strong> ${dado.titulo}</li>
+                        <li><strong>Artista(s):</strong> ${dado.artista}</li>
+                        <li><strong>Álbum:</strong> ${dado.album}</li>
+                        <li><strong>Data de lançamento:</strong> ${dado.lancamento}</li>
+                        <li><strong>Gênero musical:</strong> ${dado.genero}</li>
+                    </ul>
+                </div>
+
+                <div class="letmusic">
+                    <h2>
+                        ${dado.titulo}
+                    </h2>
+                    <p class="descricao-meta"> 
+                        <p>
+                            ${dado.letra}
+                        </p>
+                    </p>
+                    <a href=${dado.link} target="_blank"> Visão geral &rarr;</a>
+                </div>
+            </div>
+        `
+    }
+    // Atribui o HTML gerado à seção de resultados
+    section.innerHTML = resultado
+}
+
