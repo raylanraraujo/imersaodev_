aca4c370b4a6eef5e5239e787e90b609c210665b
Raylan Araujo <raylanra@gmail.com>
2024-09-06 10:07:17 -0300

    finalizada a aula 5 com código funcionando

--- app.js
+++ app.js
@@ -2,13 +2,35 @@ function pesquisar() {
     // Obtém a seção HTML onde os resultados serão exibidos
     let section = document.getElementById("resultados-pesquisa");
 
+    let campoPesquisa = document.getElementById("campo-pesquisa").value
+    
+    // se campoPesquisa for string sem nada
+    if (campoPesquisa =="" ){
+        section.innerHTML = "<p>Nada foi encontrado. Você precisa o nome de um atleta ou esporte.</p>"
+        return 
+    }
+
+  
+
+    campoPesquisa = campoPesquisa.toLowerCase() // vai ficar tudo minúscula e não importa como a pessoa digitar no campo de pesquisa maiúscula ou minúscula. 
+
+
+
     // Inicializa uma string para armazenar os resultados HTML
     let resultados = "";
+    let titulo ="";
+    let descricao = "";
+    let tags = "";
 
     // Itera sobre os dados da pesquisa e cria o HTML para cada resultado
     for (let dado of dados) {
-        // Cria um novo elemento HTML para cada resultado
-        resultados += `
+        titulo = dado.titulo.toLowerCase()
+        descricao = dado.descricao.toLowerCase()
+        tags = dado.tags.toLowerCase()
+        // se titulo  includes campoPesquisa
+        if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa || tags.includes(campoPesquisa))){
+            // cria um novo elemento
+            resultados += `
             <div class="item-resultado">
                 <h2>
                     <a href="#" target="_blank">${dado.titulo}</a>
@@ -17,6 +39,15 @@ function pesquisar() {
                 <a href= ${dado.link} target="_blank">Mais informações &rarr;</a>
             </div>
         `;
+        }
+            // então faça...
+        console.log(dado.titulo.includes(campoPesquisa))
+        // Cria um novo elemento HTML para cada resultado
+       
+    }
+
+    if (!resultados) {
+        resultados = "<p>Nada foi encontrado.</p>"
     }
 
     // Atualiza o conteúdo da seção com os resultados gerados
